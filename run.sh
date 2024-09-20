f064578e7e8ec6c27386704e2859c9c40534715b
Aaron Earles <aaron.earles@pediatrix.com>
2024-09-20 12:50:36 -0600

    add sleep

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 NEW_IP=$(curl -4 -s ifconfig.io)
 sed -i "s|^    ip: .*|    ip: $NEW_IP|" config.yml
 docker compose -f /bedrock/docker-compose.yml up -d
+sleep 30
 MSG=$(docker compose -f /bedrock/docker-compose.yml logs mcxboxbroadcast | grep -i microsoft.com/link | tail -n 1 | sed 's/.*\[Auth] //; s/\].*//')
 curl -d "$MSG" ntfy.sh/aearles_alerts
