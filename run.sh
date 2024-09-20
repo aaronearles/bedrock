093dcf7761fb9eb260ef55f33053a6ca8f9513f0
Aaron Earles <aaron.earles@pediatrix.com>
2024-09-20 12:43:28 -0600

    fix run.sh location and add ntfy

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 NEW_IP=$(curl -4 -s ifconfig.io)
 sed -i "s|^    ip: .*|    ip: $NEW_IP|" config.yml
-docker compose up -d
+docker compose -f /bedrock/docker-compose.yml up -d
+MSG=$(docker compose -f /bedrock/docker-compose.yml logs mcxboxbroadcast | grep -i microsoft.com/link | tail -n 1 | sed 's/.*\[Auth] //; s/\].*//')
+curl -d "$MSG" ntfy.sh/aearles_alerts
