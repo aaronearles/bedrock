8384b84ba2bc68c8c4e6e205a67bb4febad4181a
Aaron Earles <aaron.earles@pediatrix.com>
2024-09-23 10:54:32 -0600

    adjust sleep

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ wget https://github.com/MCXboxBroadcast/Broadcaster/releases/download/58/MCXboxB
 NEW_IP=$(curl -4 -s ifconfig.io)
 sed -i "s|^    ip: .*|    ip: $NEW_IP|" /bedrock/config.yml
 docker compose -f /bedrock/docker-compose.yml up -d
-sleep 10
+sleep 20
 MSG=$(docker compose -f /bedrock/docker-compose.yml logs mcxboxbroadcast | grep -i microsoft.com/link | tail -n 1 | sed 's/.*\[Auth] //; s/\].*//')
 curl -d "$MSG" ntfy.sh/aearles_alerts
