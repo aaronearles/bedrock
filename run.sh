e48db4bd85382065a70f81aa75b3ca665a6d45dc
Aaron Earles <aaron.earles@pediatrix.com>
2024-09-23 16:09:17 -0600

    testing remote broadcast local server

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget https://github.com/MCXboxBroadcast/Broadcaster/releases/download/58/MCXboxBroadcastStandalone.jar /bedrock/MCXboxBroadcastStandalone.jar
-NEW_IP=$(curl -4 -s ifconfig.io)
-sed -i "s|^    ip: .*|    ip: $NEW_IP|" /bedrock/config.yml
+#NEW_IP=$(curl -4 -s ifconfig.io)
+#sed -i "s|^    ip: .*|    ip: $NEW_IP|" /mnt/b2/earles-bedrock/config/config.yml
 docker compose -f /bedrock/docker-compose.yml up -d
-sleep 20
+sleep 30
 MSG=$(docker compose -f /bedrock/docker-compose.yml logs mcxboxbroadcast | grep -i microsoft.com/link | tail -n 1 | sed 's/.*\[Auth] //; s/\].*//')
 curl -d "$MSG" ntfy.sh/aearles_alerts
